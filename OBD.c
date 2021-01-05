4b81d6eb96bc4216f4acd0e2fbda9832c9dc128a
LenoyOst <lenoy.ost@gmail.com>
2021-01-05 11:11:31 +0200

    scanSerial : finished in case of linux operating system -- added : freeConnections - frees the the potential serial-port list -- added : send and recv - writ and read in the correct format to the elm327 -- added : connect - gets a potential serial port and tries to connect (return secsusse only if the elm327 is connected , dose not connect to other devices that may be serial ports)

--- OBD.c
+++ OBD.c
@@ -1,38 +1,44 @@
 #include "OBD.h"
 
-Connections* scanSerial(void)
-{
-	Connections* connections;
-	int i, j;
-	int current = 0;
-	char** ports;
-	
+int scanSerial(Connections* connections)
+{	
+	int error = SUCCESS;
+		
 
 #ifdef _WIN32 
+
+	int i;
+	int current = 0;
+	char** ports;
+
+	
 	HANDLE hCom;
 	OVERLAPPED o;
 	BOOL fSuccess;
 	DWORD dwEvtMask;
 	char text[11] = "\\\\.\\COM";
 
-	ports = (char**)malloc(10 * sizeof(char*));
+	ports = (char**)malloc(MAX_POTENTIALS_PORTS * sizeof(char*));
 	if (ports == NULL)
-		return NULL;
-
-	//going through all the possible ports (com 1-256)
-	for (i = 1; i < 10; i++)
 	{
-		if (i < 10) { text[7] = i + 48; }
+		free(connections);
+		return ALLOCATION_ERROR;
+	}
+		
+	//going through all the possible ports (com 0-256)
+	for (i = 0; i < 256; i++)
+	{
+		if (i < 10) { text[7] = i + '0'; }
 
 		else if (i < 100) {
-			text[7] = (i / 10) + 48;
-			text[8] = (i % 10) + 48;
+			text[7] = (i / 10) + '0';
+			text[8] = (i % 10) + '0';
 		}
 		else
 		{
-			text[7] = (i / 100) + 48;
-			text[8] = (i / 10) % 10 + 48;
-			text[9] = (i % 10) + 48;
+			text[7] = (i / 100) + '0';
+			text[8] = (i / 10) % 10 + '0';
+			text[9] = (i % 10) + '0';
 		}
 
 		hCom = CreateFile(TEXT(text),
@@ -52,36 +58,175 @@ Connections* scanSerial(void)
 		}
 		else
 		{
-			ports[current] = (char**)malloc(sizeof(text)+1);
+			ports[current] = _strdup(text);
 			if (ports[current] == NULL)
 			{
-				for (j = 0; j < current; j++)
-				{
-					free(ports[j]);
-				}
-				free(ports);
-				return NULL;
+				error = ALLOCATION_WARNING;
+				break;
+
 			}
-			ports[current] = _strdup(text);
+
 			current++;
 		}
-		if (current == 10)
+		if (current == MAX_POTENTIALS_PORTS)
 		{
+			error = RICHED_MAX_POTENTIALS_PORTS_WARNING;
 			break;
-			//toDo : warning , error
 		}
 	}
-#elif __linux__
-	//toDo
-
-#endif
-	connections = (Connections*)malloc(sizeof(Connections));
-	if (connections == NULL)
-		return NULL;
-
+	
 	connections->list = ports;
 	connections->size = current;
+#elif __linux__
+	glob_t globlist;
+	glob("/dev/ttyS[0-9]", 0, NULL, &globlist);
+	glob("/dev/ttyUSB[0-9]*", GLOB_APPEND, NULL, &globlist);
 
-	return connections;
+	connections->list = globlist.gl_pathv;
+	connections->size = globlist.gl_pathc;
+
+#endif
+	return error;
 }
 
+// -------------------------------------------------------------
+// releases the potential serial-port list(possible connections)
+// -------------------------------------------------------------
+
+void freeConnections(Connections* connection)
+{
+	int i;
+	
+	for(i=0;i<connection->size;i++)
+	{
+		free(connection->list[i]);
+	}
+	free(connection->list);
+}
+
+//---------------------------------------------------
+// trying to initialis communication with the elm327
+//---------------------------------------------------
+
+int connect(char* portname, int* pd)
+{
+	char command[100] = "sudo chmod 777 ";
+	char buf[MAX_MESSEGE_SIZE];
+	int i, error;
+	for(i=0;i<strlen(portname);i++)
+	{
+		command[i+15] = portname[i];
+	}
+	system(command);
+	*pd = open (portname, O_RDWR | O_NOCTTY | O_NDELAY);
+	if (*pd < 0)	{return NOT_OBD;}
+
+	set_interface_attribs (*pd, B38400, 0);
+	set_blocking (*pd, 1);
+	
+	error = send(*pd, "ATZ");
+	if(error>=ERROR)	{disconnect(*pd); return NOT_OBD;}
+	
+	sleep(2);
+	
+	error = recv(*pd, buf, MAX_MESSEGE_SIZE);
+	if(error >= ERROR)	{disconnect(*pd); return NOT_OBD;}
+	
+	puts(buf);
+	if(strcmp(buf, "ATZ\n\n\nELM327 v1.5\n\n>") == 0)	{return SUCCESS;}
+	
+	disconnect(*pd);
+	return NOT_OBD;
+}
+// --------------------------
+// disconnect 
+// --------------------------
+int disconnect(int pd)
+{
+	if(close(pd)<0)	{return CLOSE_ERROR;}
+	return SUCCESS;
+}
+
+// --------------------
+// send's messege to obd
+// --------------------
+
+int send(int pd, char* msg)
+{
+	int len = strlen(msg), i;
+	char text[len+3];
+	for(i=0;i<len;i++)
+	{
+		text[i] = msg[i];
+	}
+	text[i] = '\r';	
+	if(write (pd, text, strlen(text)) < 0)	{return WRITE_ERROR;}
+	return SUCCESS;
+}
+// --------------------------------------------------------------------
+// reads a massege from the connection that we created with the elm327
+// --------------------------------------------------------------------
+int recv(int pd, char* msg, int len)
+{
+	sleep(1);
+	int n;
+	if((n = read (pd, msg, len))<0)	{return READ_ERROR;}
+	msg[n] = '\0';
+	return SUCCESS;
+}
+
+int set_interface_attribs (int fd, int speed, int parity)
+{
+        struct termios tty;
+        if (tcgetattr (fd, &tty) != 0)
+        {
+		 printf("error0");
+                return -1;
+        }
+
+        cfsetospeed (&tty, speed);
+        cfsetispeed (&tty, speed);
+
+        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+        // disable IGNBRK for mismatched speed tests; otherwise receive break
+        // as \000 chars
+        tty.c_iflag &= ~IGNBRK;         // disable break processing
+        tty.c_lflag = 0;                // no signaling chars, no echo,
+                                        // no canonical processing
+        tty.c_oflag = 0;                // no remapping, no delays
+        tty.c_cc[VMIN]  = 0;            // read doesn't block
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+                                        // enable reading
+        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+        tty.c_cflag |= parity;
+        tty.c_cflag &= ~CSTOPB;
+        tty.c_cflag &= ~CRTSCTS;
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+        {
+		 printf("error1");
+                return -1;
+        }
+        return 0;
+}
+
+void set_blocking (int fd, int should_block)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof(tty));
+        if (tcgetattr (fd, &tty) != 0)
+        {
+		printf("error2");
+               return;
+        }
+
+        tty.c_cc[VMIN]  = should_block ? 1 : 0;
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+		printf("error3");
+}
--- OBD.h
+++ OBD.h
@@ -1,8 +1,51 @@
+#ifdef _WIN32 
 #include <windows.h>
 #include <tchar.h>
+#elif __linux__
+#include <glob.h>
+#endif
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h> 
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+#define MAX_POTENTIALS_PORTS 10
+#define MAX_MESSEGE_SIZE 1024
+
+// -------
+// SUCCESS
+// -------
+
+#define SUCCESS 0
+
+// --------
+// WARNINGS
+// --------
+
+#define ALLOCATION_WARNING 1
+#define RICHED_MAX_POTENTIALS_PORTS_WARNING 2
+
+// ------
+// EVENTS
+// ------
+
+#define NOT_OBD 3
+
+// ------
+// ERRORS
+// ------
+
+#define ERROR 4
+
+#define ALLOCATION_ERROR 5
+#define WRITE_ERROR 7
+#define READ_ERROR 8
+#define CLOSE_ERROR 9
 
 typedef struct connections
 {
@@ -10,4 +53,12 @@ typedef struct connections
 	int size;
 } Connections;
 
-Connections* scanSerial(void);
+int scanSerial(Connections* connections);
+void freeConnections(Connections* connection);
+int connect(char* portname, int *pd);
+void printError(int error);
+int set_interface_attribs (int fd, int speed, int parity);
+void set_blocking (int fd, int should_block);
+int send(int pd, char* msg);
+int recv(int pd, char* msg, int len);
+int disconnect(int pd);
